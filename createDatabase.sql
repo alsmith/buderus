4d516dda4e30e834f876eb7a72675a10a9905668
Al Smith <ajs@riverbed.com>
2016-07-18 15:17:50 +0200

    Remove AI statement.

--- createDatabase.sql
+++ createDatabase.sql
@@ -4,7 +4,7 @@ CREATE TABLE `buderusKeys` (
   `name` text COLLATE utf8_bin NOT NULL,
   `unit` text COLLATE utf8_bin,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 DROP TABLE IF EXISTS `buderusData`;
 CREATE TABLE `buderusData` (
@@ -13,5 +13,5 @@ CREATE TABLE `buderusData` (
   `nameId` int(11) NOT NULL,
   `value` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=203787 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 

ad2364732d951806554df10d80e7c92f451a5a4f
Al Smith <ajs@riverbed.com>
2016-07-31 14:07:43 +0200

    Adjust database-creation SQL.

--- createDatabase.sql
+++ createDatabase.sql
@@ -9,9 +9,17 @@ CREATE TABLE `buderusKeys` (
 DROP TABLE IF EXISTS `buderusData`;
 CREATE TABLE `buderusData` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `nameId` int(11) NOT NULL,
+  `timestampId` int(11) NOT NULL,
+  `keyId` int(11) NOT NULL,
   `value` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `buderusTimestamps`;
+CREATE TABLE `buderusTimestamps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `timestamp` (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
